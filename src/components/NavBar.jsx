6a93a8de324f17b060e7da226c10d0da832202aa
Kevin-Wei-KW <69333576+Whatthehacky@users.noreply.github.com>
2024-08-18 22:07:21 -0700

    Add link to spreadsheet

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import "./NavBar.css";
 import {AiFillHome} from 'react-icons/ai';
 import {BsFillBarChartFill} from 'react-icons/bs';
 import {FaMagnifyingGlass} from 'react-icons/fa6';
-import { MdOutlineRefresh, MdExitToApp, MdMoreVert } from "react-icons/md";
+import { MdOutlineRefresh, MdExitToApp, MdMoreVert, MdOpenInNew } from "react-icons/md";
 
 export default function NavBar(props) {
     const [extraMenu, setExtraMenu] = useState(false)
@@ -15,6 +15,10 @@ export default function NavBar(props) {
         await props.getStats();
     }
 
+    function goToSpreadsheet() {
+        window.open("https://docs.google.com/spreadsheets/d/15y7PRSOtRPBKAhv2jUxeXHsRyZRoo77ZqX6BP7cnx58/edit?usp=sharing", "_blank");
+    }
+
     return (
         <div className="NavBar">
             <div className={props.page=="Txns"? "nav-button selected":"nav-button"} onClick={() => props.setPage("Txns")}><AiFillHome size={40}/></div>
@@ -23,7 +27,8 @@ export default function NavBar(props) {
 
             { extraMenu &&
                 <div className="extra-menu" onClick={() => setExtraMenu(false)}>
-                    <button className="nav-extra-button" type="button" onClick={updateData}><MdOutlineRefresh size={20}/>Refresh</button>    
+                    <button className="nav-extra-button" type="button" onClick={updateData}><MdOutlineRefresh size={20}/>Refresh</button>
+                    <button className="nav-extra-button" type="button" onClick={goToSpreadsheet}><MdOpenInNew size={20}/>Spreadsheet</button>
 {/*                     <button className="nav-extra-button" type="button" onClick={props.logout}><MdExitToApp size={20}/>Log out</button>     */}
                 </div>   
             } 
